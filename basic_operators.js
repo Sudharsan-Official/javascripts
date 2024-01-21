ae2141fa067f3370173c4a5f2e90fa5083b7aeeb
Sudharsan-Official <sudharsan9946@gmail.com>
2024-01-21 21:58:00 +0530

    basics

---
+++ basic_operators.js
@@ -0,0 +1,36 @@
+console.log("I am Sudharsan");
+
+a="208"
+console.log("1." ,a)
+
+var b="sudhar"
+console.log("This is belongs to var type",b)
+b="sudharsan"
+console.log("This is belongs to var type",b)
+let c="raj"
+console.log("This is belongs to let type",c)
+const d="208"
+console.log("This is belongs to const type and it's value is:",d)
+
+//Assignment Operator
+var a = 10
+console.log("Assignment Operator Output:",a)
+
+//Arithmetic Operator
+var aA=10
+var bA=10
+console.log(typeof aA)  // For check the data type
+console.log("Arithmetic Operator Addition Output:",aA+bA)
+console.log("Arithmetic Operator Substraction Output:",aA-bA)
+console.log("Arithmetic Operator Multiplication Output:",aA*bA)
+console.log("Arithmetic Operator Division Output:",aA/bA)
+console.log("Arithmetic Operator Modulus Output:",aA%bA)
+console.log("Arithmetic Operator Output:",aA==bA)
+console.log("Arithmetic Operator Output:",aA=!bA)
+
+//Logical Operator -> && , || , ! => Output =
+var aL=10
+var bL=20
+console.log("Logical Operator && Output:",aL&&bL)
+console.log("Logical Operator || Output:",aL||bL)
+console.log("Logical Operator ! Output:",!aL)
